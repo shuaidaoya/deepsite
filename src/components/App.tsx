054c3a2d978cb996592f940a7aa834bc25d5cbdd
陈湘材 <1794130477@qq.com>
2025-04-03 08:53:58 +0800

    refactor(components): 移除部署按钮组件
    - 从 App 组件中删除了 DeployButton 的引用
    - 删除了 deploy-button 目录下的全部代码

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -17,7 +17,6 @@ import { defaultHTML } from "./../../utils/consts";
 import Tabs from "./tabs/tabs";
 import AskAI from "./ask-ai/ask-ai";
 import Preview from "./preview/preview";
-import DeployButton from "./deploy-button/deploy-button";
 
 function App() {
   const { t } = useTranslation();
@@ -245,7 +244,6 @@ function App() {
           setView={setCurrentView}
           setHtml={setHtml}
         />
-        <DeployButton />
       </main>
     </div>
   );
